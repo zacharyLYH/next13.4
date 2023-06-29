b5817bd0e88275a5b3b7a06ca5308b63d3f8c2f7
Zac <yihongle@buffalo.edu>
2023-06-29 15:14:34 -0400

    Static rendering done

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { usePathname } from "next/navigation";
+
+export default function NavBar() {
+    const pathname = usePathname();
+    return (
+        <Navbar
+            bg="primary"
+            variant="dark"
+            sticky="top"
+            expand="sm"
+            collapseOnSelect
+        >
+            <Container>
+                <Navbar.Brand as={Link} href="/">
+                    NextJS 13.4 Image Gallery
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-navbar" />
+                <Navbar.Collapse id="main-navbar">
+                    <Nav>
+                        <Nav.Link
+                            as={Link}
+                            href="/static"
+                            active={pathname === "/static"}
+                        >
+                            Static
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
--- src/app/hello/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function SayHello() {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); //simulate laoding
-    return <div>Hello NextJS 13.4</div>;
-}
